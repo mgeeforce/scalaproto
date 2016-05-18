90b1428937cbb2592581e29cdfb2260e8bf6ae31
Mike Gee <mgeeforce@gmail.com>
2016-05-18 10:53:29 -0600

    First commit. Part 1 complete.

---
+++ app/controllers/Tickets.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.json.Json
+
+class Tickets extends Controller {
+  
+  case class AvailabilityResponse(result: String, ticketQuantity: Option[Long])
+  
+  object AvailabilityResponse {
+    implicit val responseFormat = Json.format[AvailabilityResponse]
+  }
+  
+  def ticketsAvailable = Action { request => 
+    val availableTickets = 1000
+    val response = AvailabilityResponse("ok", Option(availableTickets))
+    Ok(Json.toJson(response))
+  }
+  
+}
---
+++ build.sbt
@@ -0,0 +1,17 @@
+name := """scalaproto"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  ws,
+  "org.webjars" % "jquery" % "2.2.3",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
